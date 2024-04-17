51d9f9b5057ce66f77a44afa3b7f64b37510234b
Samet Çelik <sametcelikk90@gmail.com>
2024-04-17 19:21:13 +0300

    ArtBookWithJava
    Developed with Java

---
+++ app/src/main/java/com/samet/artbookjavasamet/Art.java
@@ -0,0 +1,12 @@
+package com.samet.artbookjavasamet;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
